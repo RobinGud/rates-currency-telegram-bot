8fa14a5da9a80fe3cc415460bef07be0c4f9af7b
Nikita Matsnev <niki-sim@mail.ru>
2020-06-25 22:44:04 +0300

    first verison workable wrapper

---
+++ index.js
@@ -0,0 +1,26 @@
+const osmosis = require("osmosis");
+const fs = require("fs");
+
+let savedData = [];
+
+osmosis
+  .get("https://kovalut.ru/kurs/sankt-peterburg/")
+  .find("table.tb-k tr")
+  .set(["td"])
+  .data(function (data) {
+    if (data.length > 5) {
+      data = data.slice(0, 5);
+    }
+    if (data.length === 5) {
+      console.log(data);
+      savedData.push(data);
+    }
+  })
+  .done(function () {
+    fs.writeFile("data.json", JSON.stringify(savedData, null, 4), function (
+      err
+    ) {
+      if (err) console.error(err);
+      else console.log("Data Saved to data.json file");
+    });
+  });
