db6c78960dead94eb2c53df07ed4b32bc07048fc
Nikita Matsnev <niki-sim@mail.ru>
2020-06-26 18:05:09 +0300

    small refactoring and bug fix

--- index.js
+++ index.js
@@ -3,7 +3,6 @@ const fs = require("fs");
 
 const TelegramBot = require("node-telegram-bot-api");
 const token = process.env.BOTAPI;
-
 const bot = new TelegramBot(token, { polling: true });
 
 let fileContent = JSON.parse(fs.readFileSync("data.json", "utf8"));
@@ -15,22 +14,18 @@ bot.on("message", (msg) => {
   if (msg.text) {
     const text = msg.text.toLowerCase();
 
-    if (~text.indexOf("привет")) {
-      bot.sendMessage(chatId, "Приветик, " + first_name + "!");
-    } else if (~text.indexOf("курс доллара")) {
-      // bot.sendMessage(chatId, "Курс доллара💵");
+    if (~text.indexOf("курс доллара")) {
       for (let i = 1; i < 6; i++) {
         bot.sendMessage(
           chatId,
           fileContent[i][0] +
-            "\n\t" +
+            "\n\t Купить  " +
             fileContent[i][1] +
-            "\n\t" +
+            "\n\t Продать " +
             fileContent[i][2]
         );
       }
     } else if (~text.indexOf("курс евро")) {
-      // bot.sendMessage(chatId, "Курс Евро💶");
       for (let i = 1; i < 6; i++) {
         bot.sendMessage(
           chatId,
@@ -41,8 +36,6 @@ bot.on("message", (msg) => {
             fileContent[i][4]
         );
       }
-    } else if (~text.indexOf("клав")) {
-      openKeyboard(chatId);
     } else {
       bot.sendMessage(chatId, "Unknown command");
     }
@@ -51,8 +44,8 @@ bot.on("message", (msg) => {
 
 bot.onText(/\/start/, (msg, match) => {
   const chatId = msg.chat.id;
-  bot.sendMessage(chatId, "Приветик, " + msg.chat.first_name + "!");
   openKeyboard(chatId);
+  bot.sendMessage(chatId, "Приветик, " + msg.chat.first_name + "!");
 });
 
 const openKeyboard = (chatId) => {
