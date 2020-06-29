f7881edef79719c45db293df6f0ccbd1b1b398e4
Nikita Matsnev <niki-sim@mail.ru>
2020-06-30 01:55:14 +0300

    coming tech req. All old code useless :( Add  parse xml from crb

--- index.js
+++ index.js
@@ -5,128 +5,128 @@ const TelegramBot = require("node-telegram-bot-api");
 const token = process.env.BOTAPI;
 const bot = new TelegramBot(token, { polling: true });
 
-let BankRates = JSON.parse(fs.readFileSync("data.json", "utf8"));
-let CBRates = JSON.parse(fs.readFileSync("cb.json", "utf8")).ValCurs.Valute;
+// let BankRates = JSON.parse(fs.readFileSync("data.json", "utf8"));
+// let CBRates = JSON.parse(fs.readFileSync("cb.json", "utf8")).ValCurs.Valute;
 
-bot.on("message", (msg) => {
-  const chatId = msg.chat.id;
+// bot.on("message", (msg) => {
+//   const chatId = msg.chat.id;
 
-  if (msg.text && msg.text[0] !== "/") {
-    const text = msg.text.toLowerCase();
-    let str = "";
+//   if (msg.text && msg.text[0] !== "/") {
+//     const text = msg.text.toLowerCase();
+//     let str = "";
 
-    if (~text.indexOf("курс доллара")) {
-      for (let i = 1; i < 6; i++) {
-        str +=
-          "🏦 " +
-          BankRates[i][0] +
-          "\n\t  Купить  " +
-          BankRates[i][1] +
-          "\n\t  Продать " +
-          BankRates[i][2] +
-          "\n\n";
-      }
-    } else if (~text.indexOf("курс евро")) {
-      for (let i = 1; i < 6; i++) {
-        str +=
-          "🏦 " +
-          BankRates[i][0] +
-          "\n\t  Купить  " +
-          BankRates[i][3] +
-          "\n\t  Продать " +
-          BankRates[i][4] +
-          "\n\n";
-      }
-    } else if (~text.indexOf("курс цб")) {
-      for (let i = 0; i < CBRates.length; i++) {
-        str +=
-          "[" +
-          CBRates[i].CharCode._text +
-          "]  " +
-          CBRates[i].Name._text +
-          " x" +
-          CBRates[i].Nominal._text +
-          "\n  " +
-          CBRates[i].Value._text +
-          "\n\n";
-      }
-    } else {
-      str = "Unknown command!";
-    }
-    bot.sendMessage(chatId, str);
-  }
-});
+//     if (~text.indexOf("курс доллара")) {
+//       for (let i = 1; i < 10; i++) {
+//         str +=
+//           "🏦 " +
+//           BankRates[i][0] +
+//           "\n\t  Купить  " +
+//           BankRates[i][1] +
+//           "\n\t  Продать " +
+//           BankRates[i][2] +
+//           "\n\n";
+//       }
+//     } else if (~text.indexOf("курс евро")) {
+//       for (let i = 1; i < 10; i++) {
+//         str +=
+//           "🏦 " +
+//           BankRates[i][0] +
+//           "\n\t  Купить  " +
+//           BankRates[i][3] +
+//           "\n\t  Продать " +
+//           BankRates[i][4] +
+//           "\n\n";
+//       }
+//     } else if (~text.indexOf("курс цб")) {
+//       for (let i = 0; i < CBRates.length; i++) {
+//         str +=
+//           "[" +
+//           CBRates[i].CharCode._text +
+//           "]  " +
+//           CBRates[i].Name._text +
+//           " x" +
+//           CBRates[i].Nominal._text +
+//           "\n  " +
+//           CBRates[i].Value._text +
+//           "\n\n";
+//       }
+//     } else {
+//       str = "Unknown command!";
+//     }
+//     bot.sendMessage(chatId, str);
+//   }
+// });
 
-bot.onText(/\/start/, (msg, match) => {
-  let chatId = msg.chat.id;
-  openKeyboard(chatId);
-});
+// bot.onText(/\/start/, (msg, match) => {
+//   let chatId = msg.chat.id;
+//   openKeyboard(chatId);
+// });
 
-bot.on("polling_error", (msg) => console.log(msg));
+// bot.on("polling_error", (msg) => console.log(msg));
 
-bot.onText(/convert (\d.+) (.+) to (.+)/, function (msg, match) {
-  let userId = msg.from.id;
-  let valuteValue = match[1];
-  let valuteFrom = match[2].toUpperCase();
-  let valuteTo = match[3].toUpperCase();
-  let nominalFrom, nominalTo, valueFrom, valueTo;
+// bot.onText(/convert (\d.+) (.+) to (.+)/, function (msg, match) {
+//   let userId = msg.from.id;
+//   let valuteValue = match[1];
+//   let valuteFrom = match[2].toUpperCase();
+//   let valuteTo = match[3].toUpperCase();
+//   let nominalFrom, nominalTo, valueFrom, valueTo;
 
-  for (let i = 0; i < CBRates.length; i++) {
-    if (CBRates[i].CharCode._text == valuteFrom) {
-      nominalFrom = CBRates[i].Nominal._text;
-      valueFrom = parseFloat(
-        CBRates[i].Value._text.replace(",", ".").replace(" ", "")
-      );
-    }
+//   for (let i = 0; i < CBRates.length; i++) {
+//     if (CBRates[i].CharCode._text == valuteFrom) {
+//       nominalFrom = CBRates[i].Nominal._text;
+//       valueFrom = parseFloat(
+//         CBRates[i].Value._text.replace(",", ".").replace(" ", "")
+//       );
+//     }
 
-    if (CBRates[i].CharCode._text == valuteTo) {
-      nominalTo = CBRates[i].Nominal._text;
-      valueTo = parseFloat(
-        CBRates[i].Value._text.replace(",", ".").replace(" ", "")
-      );
-    }
-    if (valuteTo == "RUB") {
-      nominalTo = 1;
-      valueTo = 1;
-    }
+//     if (CBRates[i].CharCode._text == valuteTo) {
+//       nominalTo = CBRates[i].Nominal._text;
+//       valueTo = parseFloat(
+//         CBRates[i].Value._text.replace(",", ".").replace(" ", "")
+//       );
+//     }
+//     if (valuteTo == "RUB") {
+//       nominalTo = 1;
+//       valueTo = 1;
+//     }
 
-    if (valuteFrom == "RUB") {
-      nominalFrom = 1;
-      valueFrom = 1;
-    }
-  }
+//     if (valuteFrom == "RUB") {
+//       nominalFrom = 1;
+//       valueFrom = 1;
+//     }
+//   }
 
-  if (!valueFrom || !valueTo) {
-    bot.sendMessage(userId, "Невалидная валюта, попробуйте снова!");
-    return;
-  }
+//   if (!valueFrom || !valueTo) {
+//     bot.sendMessage(userId, "Невалидная валюта, попробуйте снова!");
+//     return;
+//   }
 
-  let Result = (
-    ((valuteValue / nominalFrom) * valueFrom * nominalTo) /
-    valueTo
-  ).toFixed(4);
+//   let Result = (
+//     ((valuteValue / nominalFrom) * valueFrom * nominalTo) /
+//     valueTo
+//   ).toFixed(4);
 
-  bot.sendMessage(userId, Result + " " + valuteTo);
-});
+//   bot.sendMessage(userId, Result + " " + valuteTo);
+// });
 
-const openKeyboard = (chatId) => {
-  bot.sendMessage(chatId, "Привет", {
-    reply_markup: {
-      keyboard: [
-        [
-          {
-            text: "Курс Доллара",
-          },
-          {
-            text: "Курс Евро",
-          },
-        ],
-        [
-          {
-            text: "Курс ЦБ",
-          },
-        ],
-      ],
-    },
-  });
-};
+// const openKeyboard = (chatId) => {
+//   bot.sendMessage(chatId, "Привет", {
+//     reply_markup: {
+//       keyboard: [
+//         [
+//           {
+//             text: "Курс Доллара",
+//           },
+//           {
+//             text: "Курс Евро",
+//           },
+//         ],
+//         [
+//           {
+//             text: "Курс ЦБ",
+//           },
+//         ],
+//       ],
+//     },
+//   });
+// };
--- rates-scrapper.js
+++ rates-scrapper.js
@@ -1,25 +1,25 @@
-const osmosis = require("osmosis");
-const fs = require("fs");
+// const osmosis = require("osmosis");
+// const fs = require("fs");
 
-let savedData = [];
+// let savedData = [];
 
-osmosis
-  .get("https://kovalut.ru/kurs/sankt-peterburg/")
-  .find("table.tb-k tr")
-  .set(["td"])
-  .data(function (data) {
-    if (data.length > 5) {
-      data = data.slice(0, 5);
-    }
-    if (data.length === 5) {
-      savedData.push(data);
-    }
-  })
-  .done(function () {
-    fs.writeFile("data.json", JSON.stringify(savedData, null, 4), function (
-      err
-    ) {
-      if (err) console.error(err);
-      else console.log("Data Saved to data.json file");
-    });
-  });
+// osmosis
+//   .get("https://kovalut.ru/kurs/sankt-peterburg/")
+//   .find("table.tb-k tr")
+//   .set(["td"])
+//   .data(function (data) {
+//     if (data.length > 5) {
+//       data = data.slice(0, 5);
+//     }
+//     if (data.length === 5) {
+//       savedData.push(data);
+//     }
+//   })
+//   .done(function () {
+//     fs.writeFile("data.json", JSON.stringify(savedData, null, 4), function (
+//       err
+//     ) {
+//       if (err) console.error(err);
+//       else console.log("Data Saved to data.json file");
+//     });
+//   });
